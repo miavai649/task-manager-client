50bbf786f8df66bf89b4da61f7ad78c248c3d9df
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-12-29 12:20:26 +0600

    add task form added

--- src/Pages/Addtask/Addtask.js
+++ src/Pages/Addtask/Addtask.js
@@ -6,13 +6,48 @@ import {
     Textarea,
     TextInput
 } from "flowbite-react";
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
 
 const Addtask = () => {
+
+    const imgHostKey = process.env.REACT_APP_imagebb_key;
+    const [startDate, setStartDate] = useState(new Date())
+    const [value, onChange] = useState('10:00');
+
+    // console.log(startDate)
+
+    const handleAddTask = e => {
+        e.preventDefault()
+        const form = e.target;
+        const title = form.title.value
+        const image = form.image.files[0]
+        const details = form.details.value
+        const date = startDate
+        const time = value
+
+        const formData = new FormData() 
+   formData.append('image', image)
+
+    const url = `https://api.imgbb.com/1/upload?key=${imgHostKey}`
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(data => console.log(data.data.url))
+        .catch(err => console.log(err))
+        
+        console.log(title, details, date, time, image)
+    }
+
   return (
     <div className="max-w-xl container mx-auto my-14">
       <Card>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleAddTask} className="flex flex-col gap-4">
           <div>
             <div className="mb-2 block">
               <Label htmlFor="text1" value="Title" />
@@ -20,6 +55,7 @@ const Addtask = () => {
             <TextInput
               id="text1"
               type="text"
+              name="title"
               placeholder="Task title"
               required={true}
             />
@@ -32,6 +68,7 @@ const Addtask = () => {
               id="comment"
               placeholder="Task details..."
               required={true}
+              name='details'
               rows={2}
             />
           </div>
@@ -39,7 +76,11 @@ const Addtask = () => {
             <div className="mb-2 block">
               <Label htmlFor="file" value="Photo" />
             </div>
-            <FileInput id="file" />
+            <FileInput name="image" id="file" />
+          </div>
+          <div className="flex justify-between">
+            <div><DatePicker selected={startDate} dateFormat="PP" onChange={(date) => setStartDate(date)} /></div>
+            <div><TimePicker onChange={onChange} value={value} /></div>
           </div>
           <Button type="submit">Submit</Button>
         </form>
